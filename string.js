9eaa416355cd850cf49e8fa7c99ed69cffc81f6e
Raghul <raghulnov13@gmail.com>
2022-09-12 08:23:01 +0530

    file

---
+++ string.js
@@ -0,0 +1,9 @@
+// let str = "Hello@raghul welcome@India";
+// str = str.replace(/@/g, "");
+// console.log(str);
+
+
+// slice
+let str = "Hello@raghul";
+str = str.slice(0, 5) +str.slice(6, str.length);
+console.log(str);
